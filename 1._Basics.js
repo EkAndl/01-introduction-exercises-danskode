fb622e4b91f34b3930b1803101ba90c82e9b0b50
danskode <149320369+danskode@users.noreply.github.com>
2025-08-28 11:39:21 +0200

    Opgave 1

--- 1._Basics.js
+++ 1._Basics.js
@@ -10,6 +10,7 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+console.log(`My firstname is ${firstName} and my lastname is ${lastName}`);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -21,5 +22,16 @@ const increment = 1;
 // The result should be 2025
 // You cannot touch the first or the second line
 
+const currentYear1 = Number(year) + increment; // ikke typecasting i js, men at parse
+const currentYear2 = parseInt(year) + increment;
+const currentYear3 = +year + increment;
+
+console.log(currentYear1); // ikke regne i console ...
+console.log(currentYear2);
+console.log(currentYear3);
+
+const possibleNumber = "323f432fef";
+console.log(Number(possibleNumber)); // NotaNumber NaN
+console.log(parseInt(possibleNumber)); // Tager tal til første bogstav
 
 // --------------------------------------

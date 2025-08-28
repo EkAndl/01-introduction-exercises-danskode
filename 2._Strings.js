0522b88f71c02607818679678a2611b4e6ffb007
danskode <149320369+danskode@users.noreply.github.com>
2025-08-28 22:02:03 +0200

    Finished all tasks now.

--- 2._Strings.js
+++ 2._Strings.js
@@ -7,6 +7,11 @@ const numberTwo = "2.30";
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
+const numberOneFloat = parseFloat(numberOne);
+const numberTwoFloat = parseFloat(numberTwo);
+const resultFloat = numberOneFloat + numberTwoFloat;
+
+console.log(resultFloat);
 
 // --------------------------------------
 // Exercise 4 - Add the numbers and the total with 2 decimals
@@ -14,6 +19,12 @@ const numberTwo = "2.30";
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
+const anotherNumberOneFloat = parseFloat(anotherNumberOne);
+const anotherNumberTwoFloat = parseFloat(anotherNumberTwo);
+
+const resultAnotherFloat = (anotherNumberOneFloat + anotherNumberTwoFloat).toFixed(2);
+
+console.log(resultAnotherFloat);
 
 // --------------------------------------
 // Exercise 5 - Decimals and average
@@ -24,14 +35,19 @@ const three = 98;
 
 // Show in the console the avg. with 5 decimals
 
+const resultWithFiveDecimals = parseFloat( (one + two + three) / 3 ).toFixed(5);
 
-
+console.log(resultWithFiveDecimals);
 
 // --------------------------------------
 // Exercise 6 - Get the character by index
 
 const letters = "abc";
 // Get me the character "c"
+
+const findTheC = letters.lastIndexOf("c");
+
+console.log(findTheC);
 
 
 
@@ -43,6 +59,10 @@ const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
 
+const findIndexOfJToBigify = fact.lastIndexOf("j");
+const bigFact = fact.slice(0, findIndexOfJToBigify) + fact.charAt(findIndexOfJToBigify).toUpperCase() + fact.slice(findIndexOfJToBigify + 1);
+
+console.log(bigFact);
 
 // --------------------------------------
 
